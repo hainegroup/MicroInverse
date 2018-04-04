8b6c51c0bebed1f1d30e9421b1b442b1a56457d1
AleksiNummelin <aleksi.h.nummelin@gmail.com>
2018-04-04 18:31:39 -0400

    Changed testing

--- tests/test_MicroInverse.py
+++ tests/test_MicroInverse.py
@@ -7,28 +7,32 @@
 import unittest
 from click.testing import CliRunner
 
-from MicroInverse import MicroInverse
+from MicroInverse import mutils #MicroInverse
+from MicroInverse import run_examples
 from MicroInverse import cli
 
 
 class TestMicroinverse(unittest.TestCase):
     """Tests for `MicroInverse` package."""
+    
+    def test_000(self):
+        run_examples('running examples...')
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'MicroInverse.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+    #def setUp(self):
+    #    """Set up test fixtures, if any."""
+    #
+    #def tearDown(self):
+    #    """Tear down test fixtures, if any."""
+    #
+    #def test_000_something(self):
+    #    """Test something."""
+    #
+    #def test_command_line_interface(self):
+    #    """Test the CLI."""
+    #    runner = CliRunner()
+    #    result = runner.invoke(cli.main)
+    #    assert result.exit_code == 0
+    #    assert 'MicroInverse.cli.main' in result.output
+    #    help_result = runner.invoke(cli.main, ['--help'])
+    #    assert help_result.exit_code == 0
+    #    assert '--help  Show this message and exit.' in help_result.output
