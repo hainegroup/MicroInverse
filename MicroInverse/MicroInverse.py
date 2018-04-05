7c962c919a6815f29fe109ea5bdd13ab4c002dda
AleksiNummelin <aleksi.h.nummelin@gmail.com>
2018-04-05 09:42:19 -0400

    modifying examples

--- MicroInverse/MicroInverse.py
+++ MicroInverse/MicroInverse.py
@@ -3,8 +3,8 @@
 """Main module. Here we simply """
 
 import MicroInverse_utils as mutils
-
-def run_examples(instring):
-    print(instring)
-    execfile('MicroInverse_examples.py')
-    print('done!')
+from MicroInverse_examples import run_examples
+#def run_examples(instring):
+#    print(instring)
+#    execfile('MicroInverse_examples.py')
+#    print('done!')
